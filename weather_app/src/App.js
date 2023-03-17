e48ba2241071358caa038dfb0e25e966b87bec98
sidharth baby <sidharthbaby77@gmail.com>
2023-03-17 08:10:52 +0530

    code setup

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -5,8 +5,7 @@ import axios from "axios";
 function App() {
   const [data, setdata] = useState({});
   const [location, setLocation] = useState("");
-  const url =
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=643d13fd6879479a391778e21f97af3e`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=643d13fd6879479a391778e21f97af3e`;
 
   // const weatherAPi = axios.fetch("");
   const searchLocation = (event) => {
@@ -15,11 +14,11 @@ function App() {
         setdata(response.data);
         console.log(response.data);
       });
-      setLocation('')
+      setLocation("");
     }
   };
   return (
-    <div className="app">   
+    <div className="app">
       <div className="search">
         <input
           type="text"
@@ -27,35 +26,43 @@ function App() {
           onChange={(e) => {
             setLocation(e.target.value);
           }}
-          placeholder="enter location"
+          placeholder="Enter Location"
           onKeyPress={searchLocation}
         />
       </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>afjhakh</p>
+            <p>{data ? data.location : ""}</p>
           </div>
           <div className="temp">
-            <h1>60* F</h1>
+            {data.main ? <h1>{data.main.temp.toFixed()}* F </h1> : ""}
           </div>
           <div className="description">
-            <p>clouds</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
-        <div className="bottom">
-          <div className="feels">
-            <p className="bold">65 *F</p>
-            <p>Feels like </p>
+        {data.name != undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p>{data.main.feels_like.toFixed()} *F</p> : null}
+
+              <p>Feels Like </p>
+            </div>
+            <div className="humidity">
+              {data.main ? (
+                <p className="bold">{data.main.humidity} %</p>
+              ) : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-          <div className="humidity">
-            <p className="bold">20%</p>
-          </div>
-          <div className="wind">
-            <p className="bold">12mph </p>
-            <p>wind speed</p>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
